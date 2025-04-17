916ad6e0362e2b22fc618d5a13d8db43d61e3409
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-17 13:36:44 +0300

    models.py

--- cinema/models.py
+++ cinema/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -7,11 +8,13 @@ class Actor(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
@@ -20,6 +23,7 @@ class CinemaHall(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
