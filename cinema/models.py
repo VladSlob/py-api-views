ed5938a7dbf0fc767a8f669843c6095f07f12389
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-22 10:02:08 +0300

    models.py

--- cinema/models.py
+++ cinema/models.py
@@ -2,32 +2,26 @@ from django.db import models
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=255, unique=True)
 
 
 class CinemaHall(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.name}"
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(related_name="actors")
-    genres = models.ManyToManyField(related_name="genres")
     duration = models.IntegerField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.title
