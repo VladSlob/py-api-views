eab72d8f2f8bf24305b9f22ca1479f6ce39f173d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-22 09:58:17 +0300

    models.py

--- cinema/models.py
+++ cinema/models.py
@@ -2,33 +2,21 @@ from django.db import models
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-
-    class Meta:
-        ordering = ["first_name", "last_name"]
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    class Meta:
-        ordering = ["name"]
-
-    def __str__(self):
-        return f"{self.name}"
+    name = models.CharField(max_length=100, unique=True)
 
 
 class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
-
-    class Meta:
-        ordering = ["name"]
 
     def __str__(self):
         return f"{self.name}"
@@ -37,12 +25,10 @@ class CinemaHall(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies")
-    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(related_name="actors")
+    genres = models.ManyToManyField(related_name="genres")
     duration = models.IntegerField()
-
-    class Meta:
-        ordering = ["title"]
 
     def __str__(self):
         return self.title
+
