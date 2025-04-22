e07f80756fcceda6847caa8755d308fd95414d00
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-22 09:48:19 +0300

    serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,101 +3,41 @@ from rest_framework import serializers
 from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
-    actors = serializers.ListField(
-        child=serializers.IntegerField(), write_only=True
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all()
     )
-    genres = serializers.ListField(
-        child=serializers.IntegerField(), write_only=True
+    actors = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Actor.objects.all()
     )
 
-    def create(self, validated_data):
-        actors_data = validated_data.pop("actors", [])
-        genres_data = validated_data.pop("genres", [])
-        movie = Movie.objects.create(**validated_data)
-        movie.actors.set(actors_data)
-        movie.genres.set(genres_data)
-        return movie
-
-    def update(self, instance, validated_data):
-        actors_data = validated_data.pop("actors", None)
-        genres_data = validated_data.pop("genres", None)
-
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
+    class Meta:
+        model = Movie
+        fields = [
+            "id",
+            "title",
             "description",
-            instance.description
-        )
-        instance.duration = validated_data.get("duration", instance.duration)
-        instance.save()
-
-        if actors_data is not None:
-            instance.actors.set(actors_data)
-        if genres_data is not None:
-            instance.genres.set(genres_data)
-
-        return instance
-
-
-class ActorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=255)
-    last_name = serializers.CharField(max_length=255)
-
-    def create(self, validated_data):
-        return Actor.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get(
-            "first_name",
-            instance.first_name
-        )
-        instance.last_name = validated_data.get(
-            "last_name",
-            instance.last_name
-        )
-
-        instance.save()
-
-        return instance
-
-
-class GenreSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-
-    def create(self, validated_data):
-        return Genre.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-
-        instance.save()
-
-        return instance
-
-
-class CinemaHallSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    rows = serializers.IntegerField()
-    seats_in_row = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return CinemaHall.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get(
-            "seats_in_row",
-            instance.seats_in_row
-        )
-
-        instance.save()
-
-        return instance
+            "duration",
+            "genres",
+            "actors"
+        ]
